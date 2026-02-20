40fb22a3b66cab5af1b20a54c776ce979d799aeb
ImAguss <jellow022@gmail.com>
2026-02-20 15:21:23 -0300

    a

--- hypr/scripts/iniciar_IA.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if  ! sudo systemctl is-active --quiet open-webui ; then
-    sudo systemctl start open-webui
-fi
-
-hyprctl dispatch exec "[workspace 5 silent] kitty -e ollama serve"
-sleep 5
-hyprctl dispatch exec "firefox http://localhost:8080"
